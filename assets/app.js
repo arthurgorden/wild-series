ddae54350636209a51da5e1db4691ebae3ac5385
agorden <arthur.gorden@live.fr>
2022-05-24 14:38:52 +0200

    symfony quest 4 done

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,21 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+
+// Webpack Encore test
+console.log('Hello Webpack Encore !');
 
 // start the Stimulus application
 import './bootstrap';
+
+// returns the final, public path to this file
+// path is relative to this file - e.g. assets/images/logo.png
+import logoPath from './images/WildSeriesMainLogo.svg';
+
+let html = `<img src="${logoPath}" alt="Wild Series Main logo">`;
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
