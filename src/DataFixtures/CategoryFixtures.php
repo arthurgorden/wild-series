52b137989ab1b972dd722f521775c7fdfeb03b35
agorden <arthur.gorden@live.fr>
2022-05-27 17:41:06 +0200

    symfony quest 7 fixtures done

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    const CATEGORIES = [
+        'Action',
+        'Actualité',
+        'Aventure',
+        'Animation',
+        'Anime',
+        'Comédie',
+        'Comédie Musicale',
+        'Crime',
+        'Documentaire',
+        'Drame',
+        'Fantastique',
+        'Guerre',
+        'Historique',
+        'Horreur',
+        'Indie',
+        'Romantique',
+        'Science-Fiction',
+        'Suspense',
+        'Thriller',
+        'Western'
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach (SELF::CATEGORIES as $key => $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+        }
+        $manager->flush();
+    }
+}
