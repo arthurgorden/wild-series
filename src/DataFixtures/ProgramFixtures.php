8bc523dcbf858c9b0fe629f7688c178d5666df8b
agorden <arthur.gorden@live.fr>
2022-05-28 12:27:43 +0200

    symfony quest 9 fixtures advanced done

---
+++ src/DataFixtures/ProgramFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Program;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ProgramFixtures extends Fixture implements DependentFixtureInterface
+{
+    const ACTION_PROGRAMS = [
+        'La Symfony du Nouveau Monde',
+        'Symfony en mi bémol',
+        'Symfony en mie de pain',
+        'Symfony',
+        'Symfonin the rain',
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::ACTION_PROGRAMS as $key => $programName) {
+            $program = new Program();
+            $program->setTitle($programName);
+            $program->setSynopsis('Synopsis ' . $key);
+            $program->setCategory($this->getReference('category_Action'));
+            $manager->persist($program);
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        // Tu retournes ici toutes les classes de fixtures dont ProgramFixtures dépend
+        return [
+          CategoryFixtures::class,
+        ];
+    }
+}
